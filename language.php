f153dbf8dfe963b26c1a85d41397284ffbd37091
slamflava <slam@mail.ru>
2017-04-01 21:35:18 +0400

    ssss

---
+++ language.php
@@ -0,0 +1,57 @@
+<?php
+
+$lang_album['rus'] = "Альбом";
+$lang_video['rus'] = "Видео";
+$lang_music['rus'] = "Музыка";
+$lang_artist['rus'] = "Исполнитель";
+$lang_artists['rus'] = "Исполнители";
+$lang_all_artists['rus'] = "Все исполнители";
+$lang_year['rus'] = "Год";
+$lang_quality['rus'] = "Качество";
+$lang_audio_quality['rus'] = "Качество аудио";
+$lang_video_quality['rus'] = "Качество видео";
+$lang_tracks['rus'] = "Количество треков";
+$lang_tracklist['rus'] = "Треклист";
+$lang_password['rus'] = "Пароль на архив";
+$lang_download['rus'] = "Скачать";
+$lang_albums['rus'] = "Альбомов";
+$lang_vas['rus'] = "Сборников";
+$lang_osts['rus'] = "Саундтреков";
+$lang_videos['rus'] = "Видеоклипов";
+$lang_new_stuff['rus'] = "Новый стафф";
+$lang_search['rus'] = "Поиск";
+$lang_page['rus'] = "Страница";
+$lang_of['rus'] = "из";
+$lang_first['rus'] = "Первая";
+$lang_last['rus'] = "Последняя";
+$lang_disclaimer['rus'] = "Весь музыкальный контент, находящийся на данном сайте, выложен исключительно для ознакомительных целей.<br />Поддерживайте исполнителей, покупайте только лицензионную продукцию!";
+$lang_supported['rus'] = "при поддержке";
+
+$lang_album['eng'] = "Album";
+$lang_video['eng'] = "Video";
+$lang_music['eng'] = "Music";
+$lang_artist['eng'] = "Artist";
+$lang_artists['eng'] = "Artists";
+$lang_all_artists['eng'] = "All Artists";
+$lang_year['eng'] = "Year";
+$lang_quality['eng'] = "Quality";
+$lang_audio_quality['eng'] = "Audio Quality";
+$lang_video_quality['eng'] = "Video Quality";
+$lang_tracks['eng'] = "Tracks";
+$lang_tracklist['eng'] = "Tracklist";
+$lang_password['eng'] = "Password";
+$lang_download['eng'] = "Download";
+$lang_albums['eng'] = "Albums";
+$lang_vas['eng'] = "Various Artists";
+$lang_osts['eng'] = "Soundtracks";
+$lang_videos['eng'] = "Videos";
+$lang_new_stuff['eng'] = "New Stuff";
+$lang_search['eng'] = "Search";
+$lang_page['eng'] = "Page";
+$lang_of['eng'] = "of";
+$lang_first['eng'] = "First";
+$lang_last['eng'] = "Last";
+$lang_disclaimer['eng'] = "All music contains on this site is only for private review.<br />Support artists, buy their CDs.";
+$lang_supported['eng'] = "supported by";
+
+?>
